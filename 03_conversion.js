f6baf437741c9a6567f6912c4fe502986605de94
devyanipathak <devyanipthk@gmail.com>
2023-12-25 14:07:11 +0530

    conversion

---
+++ 03_conversion.js
@@ -0,0 +1,25 @@
+// let score = true // 3, "3", "3abc", null, true
+
+// console.log(typeof(score));
+// console.log(typeof score);
+
+// let valueInNumber = Number(score);  //Number is object format of number it should be written in capital.
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);//("3abc", "Devyani")NaN : Not a number it's a special type
+//"33" => 33
+//"33abc" => NaN
+//null => 0
+//true => 1; false => 0
+
+let isLoggedIn =  "Deva"
+let booleanIsLoggedIn = Boolean(isLoggedIn);
+console.log(booleanIsLoggedIn);
+//1 =>true; 0 => false
+// "" =>false;
+//"hitesh" =>true;
+
+let someNumber = 33
+
+let stringNumber = String(someNumber);
+console.log(stringNumber);
+console.log(typeof stringNumber);
