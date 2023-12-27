cca2b90a79dbd710ee97af2a419e05dfd447fbbd
devyanipathak <devyanipthk@gmail.com>
2023-12-27 16:56:00 +0530

    Numbers and Maths

---
+++ 08_numbers.js
@@ -0,0 +1,38 @@
+// const score = 400
+// console.log(score);
+
+// const balance = new Number(100)
+// console.log(balance);
+
+// console.log(balance.toString().length);
+//To get priority of numbers after decimal
+// console.log(balance.toFixed(2));
+
+// const otherNumber = 123.8966
+// //to get priority of numbers mainly before decimal.
+// console.log(otherNumber.toPrecision(5));
+
+// const hundreds =1000000
+// console.log(hundreds.toLocaleString('en-IN'));
+
+
+//+++++++++++++++++++++++++Maths+++++++++++++++++++++++++++++
+
+// console.log(Math);
+// console.log(Math.abs(-4));
+// console.log(Math.round(4.6));
+// console.log(Math.ceil(4.6));
+// console.log(Math.floor(4.6));
+// console.log(Math.min(4,3,1,2,0));
+// console.log(Math.max(4,3,1,2,0));
+
+
+console.log(Math.random());//gives value between 0-1
+console.log((Math.random()*10)+1);//*10 shifts value in left 
+console.log(Math.floor(Math.random()*10)+1);//gives value between 0-9
+
+const min=20
+const max=30
+console.log(Math.floor(Math.random() * (max-min+1) + min));//to get value between given range.
+
+
